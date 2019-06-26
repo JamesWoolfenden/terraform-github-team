3cc5e91b46ca034e72373d89d9f043ee4132c966
James Woolfenden <James.Woolfenden@slalom.com>
2019-06-26 18:10:20 +0100

    draft

---
+++ github_membership.org.tf
@@ -0,0 +1,5 @@
+resource "github_membership" "org" {
+  count    = "${length(var.users)}"
+  username = "${lookup(var.users[count.index],"username")}"
+  role     = "${lookup(var.users[count.index],"role")}"
+}
---
+++ github_team.team.tf
@@ -0,0 +1,6 @@
+# Add a team to the organization
+resource "github_team" "team" {
+  name        = "${var.team_name}"
+  description = "${var.team_description}"
+  privacy     = "${var.team_privacy}"
+}
